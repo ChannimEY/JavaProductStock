85aecd45ad66d7cda42a5a73e64b9cfa8149f277
ChannimEY <channimey1@gmail.com>
2025-04-11 14:04:40 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int row,col;
+        String[][] stock= new String[][];
+        System.out.println("Welcome to java Product Stock Management");
+        System.out.println("=".repeat(20));
+        System.out.println("""
+                1.Set up stock
+                2.view stock
+                3.insert
+                4.update
+                5.delete
+                6.View insert History
+                7.Exit
+                """);
+
+        System.out.println("[+] Enter your option : ");
+        int option=new Scanner(System.in).nextInt();
+        switch (option){
+            case 1->
+            {
+
+
+            }
+        }
+        System.out.println("=".repeat(20));
+    }
+}
